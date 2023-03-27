2d307526117110bc6c51aa98ee4f33591ac618b5
qmonplaisir <qmonplaisir@gmail.com>
2023-03-27 16:02:36 -0400

    Added code to send contact form data to API

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-const formEl = document.querySelector('.contactform');
+// Send contact form to API
+const contact = document.querySelector('.contactform');
 
-formEl.addEventListener('submit', event => {
+contact.addEventListener('submit', event => {
     event.preventDefault();
+
+    const formData = new FormData(contact);
+    const data = Object.fromEntries(formData);
+
+    fetch('https://getpantry.cloud/apiv1/pantry/c71cd673-64e5-44a9-b1ab-2bd00639c06c/basket/testBasket', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error));
 });
